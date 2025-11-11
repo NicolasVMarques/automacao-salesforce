26ec3310d748515202ba70b4e6fa11b2a89cae87
NicolasVMarques <nicolas.marques26@hotmail.com>
2025-11-11 13:22:14 -0300

    feat: tratamento de exceções e registro de status

--- automacao.py
+++ automacao.py
@@ -1,19 +1,23 @@
 import pyautogui
 import time
 import pandas as pd
+from pyautogui import ImageNotFoundException
 
-tabela = pd.read_excel('Cópia de modelo para robo indicação.xlsx')
+tabela = pd.read_excel('lemit_interessados 11-11-25 Parte1.xlsx')
 
 campo_primeiro_nome_x, campo_primeiro_nome_y = 385, 356
-campo_sobrenome_x, campo_sobrenome_y = 396, 404
+campo_sobrenome_x, campo_sobrenome_y = 1076, 413
 campo_email_x, campo_email_y = 360, 490
 campo_telefone_x, campo_telefone_y = 1074, 489
-campo_razao_social_x, campo_razao_social_y = 544, 575
-campo_cnpj_x, campo_cnpj_y = 842, 575
+campo_razao_social_x, campo_razao_social_y = 427, 545
+campo_cnpj_x, campo_cnpj_y = 771, 546
 botao_confirmar_x, botao_confirmar_y = 683, 618
 botao_leads_x, botao_leads_y = 591, 175
 botao_novo_lead_x, botao_novo_lead_y = 674, 236
+botao_cancelar_x, botao_cancelar_y = 1143, 118
 email_padrao = 'empresario@gmail.com'
+
+tabela["STATUS"] = ""
 
 print("Iniciando automação em 5 segundos...")
 print("Posicione a janela do formulário e não mexa no mouse!")
@@ -63,8 +67,38 @@ for index, row in tabela.iterrows():
     time.sleep(0.3)
     pyautogui.write(email_final, interval=0.05)
 
-    print("Clicando em 'Salvar'...")
+    print("Clicando em 'Confirmar'...")
     pyautogui.click(botao_confirmar_x, botao_confirmar_y)
+    time.sleep(10)
 
-    print("Aguardando 3 segundos para o próximo registro...")
-    time.sleep(3)
+    try:
+    
+        cnpj_indicado = pyautogui.locateOnScreen('print_cnpj_indicado.png',confidence=0.55)
+
+        if cnpj_indicado:
+            print(f'Contato {contato} já estava indicado!')
+            tabela.at[index, "STATUS"] = "Já era indicado"
+
+            pyautogui.click(botao_cancelar_x, botao_cancelar_y)
+            time.sleep(3)
+
+        else:
+            print(f"Lead {contato} indicado com sucesso!")
+            tabela.at[index, "STATUS"] = "Indicado pelo Robô"
+
+    except ImageNotFoundException:
+    
+        print(f"Lead {contato} indicado (imagem não encontrada, OK)!")
+        tabela.at[index, "STATUS"] = "Indicado pelo Robô"
+
+    print("Aguardando 20 segundos para o próximo registro...")
+    time.sleep(15)
+
+    pyautogui.click(botao_leads_x, botao_leads_y)
+    time.sleep(5)
+    
+    pyautogui.click(botao_novo_lead_x, botao_novo_lead_y)
+    time.sleep(5)
+
+print('Todos os leads foram indicados com sucesso!')
+tabela.to_excel("lemit_interessados 11-11-25 Parte1 (ROBO).xlsx", index=False)
